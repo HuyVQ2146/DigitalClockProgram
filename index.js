1a30a3c921f9dfbaa4d9dfe6a2a2fbe0fcfd0937
Vu Quang Huy <Huy.VQ2421184@sis.hust.edu.vn>
2025-03-06 13:06:30 +0700

    program

---
+++ index.js
@@ -0,0 +1,15 @@
+function updateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours>=12 ? "PM" : "AM";
+    hours = hours % 12 || 12; // nếu = 12 thì hiện là 12
+    hours = hours.toString().padStart(2, 0)  
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const time = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById('clock').textContent = time;
+}
+
+updateClock();
+setInterval(updateClock, 1000) 
+// giống setTimeout() nma chạy liên tục
